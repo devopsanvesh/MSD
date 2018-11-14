1fdd4817dd09a55879d8d01019b6da320509d5d4
anvesh <anvesht2053@gmail.com>
2018-11-14 18:50:02 +0000

    MSD xommit

---
+++ src/main/java/V 1/0/App.java
@@ -0,0 +1,17 @@
+package V 1.0;
+
+public class App {
+	
+    public static void main(String[] args){
+    	new App().start();
+    }
+    
+    public App() {
+    	
+	}
+    
+    public void start() {
+
+	}
+    
+}
---
+++ src/test/java/V 1/0/AppTest.java
@@ -0,0 +1,14 @@
+package V 1.0;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+	@Test
+	void test() {
+
+	}
+
+}
+
